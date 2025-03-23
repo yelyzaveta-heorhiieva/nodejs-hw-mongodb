a853dfc3ee2040f3568ce2cd26c38e0937b9d393
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2025-03-23 16:33:29 +0200

    fix message

--- src/server.js
+++ src/server.js
@@ -45,7 +45,7 @@ export const setupServer = () => {
 
       res.status(200).json({
         status: 200,
-        message: 'Successfully found contact with id {contactId}!',
+        message: `Successfully found contact with id ${contactId}!`,
         data: contact,
       });
     });
