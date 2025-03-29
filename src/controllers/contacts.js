6b9a31eb951ac7fb6b854db9aa8cd30529dda757
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2025-03-29 22:31:47 +0200

    fix task

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -10,7 +10,7 @@ import createHttpError from 'http-errors';
 export const getContactsController = async (req, res) => {
   const contacts = await getAllContacts();
 
-  res.json({
+  res.status(200).json({
     status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
@@ -25,7 +25,7 @@ export const getContactByIdController = async (req, res, next) => {
     throw createHttpError(404, 'Contact not found');
   }
 
-  res.json({
+  res.status(200).json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
@@ -35,7 +35,7 @@ export const getContactByIdController = async (req, res, next) => {
 export const createContactController = async (req, res) => {
   const contact = await createContact(req.body);
 
-  res.json({
+  res.status(201).json({
     status: 201,
     message: `Successfully created a contact!`,
     data: contact,
@@ -51,7 +51,7 @@ export const patchContactController = async (req, res, next) => {
     throw createHttpError(404, 'Contact not found');
   }
 
-  res.json({
+  res.status(200).json({
     status: 200,
     message: `Successfully patched a contact!`,
     data: result,
