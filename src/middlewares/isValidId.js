bae7f3485a183e9009d94955e4dcfff7289b445b
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2025-04-04 15:44:45 +0300

    change error message for id validation

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -4,7 +4,7 @@ import createHttpError from 'http-errors';
 export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
   if (!isValidObjectId(contactId)) {
-    throw createHttpError(400, 'Bad Request');
+    throw createHttpError(400, 'This id is not valid');
   }
 
   next();
